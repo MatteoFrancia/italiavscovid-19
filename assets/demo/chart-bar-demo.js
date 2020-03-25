8709c3ee13e30c839fd649a12befad7db257eaaa
Matteo Francia <matteo.francia@gmail.com>
2020-03-25 16:09:56 +0100

    Overview cards refactored

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -1,50 +1,48 @@
-function totaleContagiPerGiorno(labels, data){
-  // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
+function totaleContagiPerGiorno(labels, data) {
+  Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+  Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Bar Chart Example
-var ctx = document.getElementById("overviewBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: "Contagiati",
-      backgroundColor: "rgba(255,15,15,0.3)",
-      borderColor: "rgba(255,15,15,1)",
-      borderWidth: 2,
-      data: data,
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: Math.max.apply(null, data),
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
+  var ctx = document.getElementById("overviewBarChart");
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Contagiati",
+        backgroundColor: "rgba(255,15,15,0.3)",
+        borderColor: "rgba(255,15,15,1)",
+        borderWidth: 2,
+        data: data,
       }],
     },
-    legend: {
-      display: false
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'month'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 6
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: Math.max.apply(null, data),
+            maxTicksLimit: 5
+          },
+          gridLines: {
+            display: true
+          }
+        }],
+      },
+      legend: {
+        display: false
+      }
     }
-  }
-});
+  });
 
 }
--- js/pcm-dpc.js
+++ js/pcm-dpc.js
@@ -1,21 +1,6 @@
 $(document).ready(function () {
-
-    odierno_nazionale();
     storico_nazionale();
 });
-
-function odierno_nazionale() {
-    $.getJSON(
-        "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json",
-        function (dati_pcm_dpc) {
-            document.getElementById("totale_casi").innerHTML = dati_pcm_dpc[0].totale_casi;
-            document.getElementById("totale_attualmente_positivi").innerHTML = dati_pcm_dpc[0].totale_attualmente_positivi;
-            document.getElementById("dimessi_guariti").innerHTML = dati_pcm_dpc[0].dimessi_guariti;
-            document.getElementById("deceduti").innerHTML = dati_pcm_dpc[0].deceduti;
-            nuova_data = dati_pcm_dpc[0].data.split(" ")[0].split("-")[2] + "/" + dati_pcm_dpc[0].data.split(" ")[0].split("-")[1] + "/" + dati_pcm_dpc[0].data.split(" ")[0].split("-")[0]
-            document.getElementById("data").innerHTML = "Quadro generale della situazione italiana, aggiornato al " + nuova_data + ", ore " + dati_pcm_dpc[0].data.split(" ")[1];
-        });
-}
 
 function storico_nazionale() {
     $.getJSON(
@@ -26,21 +11,49 @@ function storico_nazionale() {
             var totale_casi = [];
             var totale_attualmente_positivi = [];
             var nuovi_attualmente_positivi = [];
+            var dimessi_guariti = [];
+            var deceduti = [];
 
             $.each(dati_pcm_dpc, function (idx, obj) {
                 data.push(obj.data.split(" ")[0].split("-")[2] + "/" + obj.data.split(" ")[0].split("-")[1]);
                 totale_casi.push(obj.totale_casi);
                 totale_attualmente_positivi.push(obj.totale_attualmente_positivi);
                 nuovi_attualmente_positivi.push(obj.nuovi_attualmente_positivi);
+                dimessi_guariti.push(obj.dimessi_guariti);
+                deceduti.push(obj.deceduti);
             });
-            
-            graficoVariazioneGiornalieraContagi(data, nuovi_attualmente_positivi);
-            totaleContagiPerGiorno(data,totale_attualmente_positivi);
 
-            /* console.log(data_map); */
-            /* console.log(totale_casi_map); */
-            /* console.log(totale_attualmente_positivi_map); */
-            /* console.log(nuovi_attualmente_positivi_map); */
-            /* console.log("============="); */
+            card_riassuntive(data, totale_casi, totale_attualmente_positivi, dimessi_guariti, deceduti);
+            graficoVariazioneGiornalieraContagi(data, nuovi_attualmente_positivi);
+            totaleContagiPerGiorno(data, totale_attualmente_positivi);
         });
+}
+
+function card_riassuntive(data, totale_casi, totale_attualmente_positivi, dimessi_guariti, deceduti) {
+    totale_casi_oggi = totale_casi[totale_casi.length - 1];
+    totale_attualmente_positivi_oggi = totale_attualmente_positivi[totale_attualmente_positivi.length - 1];
+    dimessi_guariti_oggi = dimessi_guariti[dimessi_guariti.length - 1];
+    deceduti_oggi = deceduti[deceduti.length - 1];
+
+    totale_casi_ieri = totale_casi[totale_casi.length - 2];
+    totale_attualmente_positivi_ieri = totale_attualmente_positivi[totale_attualmente_positivi.length - 2];
+    dimessi_guariti_ieri = dimessi_guariti[dimessi_guariti.length - 2];
+    deceduti_ieri = deceduti[deceduti.length - 2];
+    
+    totale_casi_differenza = totale_casi_oggi - totale_casi_ieri;
+    totale_attualmente_positivi_differenza = totale_attualmente_positivi_oggi - totale_attualmente_positivi_ieri;
+    dimessi_guariti_differenza = dimessi_guariti_oggi - dimessi_guariti_ieri;
+    deceduti_differenza = deceduti_oggi - deceduti_ieri;
+
+    document.getElementById("totale_casi").innerHTML = totale_casi_oggi;
+    document.getElementById("totale_attualmente_positivi").innerHTML = totale_attualmente_positivi_oggi;
+    document.getElementById("dimessi_guariti").innerHTML = dimessi_guariti_oggi;
+    document.getElementById("deceduti").innerHTML = deceduti_oggi;
+
+    document.getElementById("totale_casi_differenza").innerHTML = (totale_casi_differenza > 0 ? "+" : "") + totale_casi_differenza;
+    document.getElementById("totale_attualmente_positivi_differenza").innerHTML = (totale_attualmente_positivi_differenza > 0 ? "+" : "") + totale_attualmente_positivi_differenza;
+    document.getElementById("dimessi_guariti_differenza").innerHTML = (dimessi_guariti_differenza > 0 ? "+" : "") + dimessi_guariti_differenza;
+    document.getElementById("deceduti_differenza").innerHTML = (deceduti_differenza > 0 ? "+" : "") + deceduti_differenza;
+
+    document.getElementById("data").innerHTML = "Quadro generale della situazione italiana, aggiornato al " + data[data.length - 1];
 }

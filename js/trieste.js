1fff56628760b57a1f7acc78b0f06d3cc169e5fd
Matteo Francia <matteo.francia@gmail.com>
2020-04-08 16:10:13 +0200

    Data updated

--- js/trieste.js
+++ js/trieste.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
         "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200404_FVG.json",
         "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200405_FVG.json",
         "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200406_FVG.json",
-        "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200407_FVG.json"
+        "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200407_FVG.json",
+        "https://raw.githubusercontent.com/MatteoFrancia/italiavscovid19/master/assets/data/20200408_FVG.json"
     ];
     var trieste = [];
     var sgonico = [];
